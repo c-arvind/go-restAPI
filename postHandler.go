7069fce4e5acf6d9f7fbc83173fbe284c2c8bada
5CITe <arch.hack28@gmail.com>
2021-10-09 23:59:52 +0530

    fixes

--- postHandler.go
+++ postHandler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"regexp"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -20,14 +21,24 @@ func NewPostHandler(postcol *mongo.Collection) *PostHandler {
 	}
 }
 
+var (
+	//listUserRe = regexp.MustCompile(`^\/users[\/]*$`)
+	getPostRe     = regexp.MustCompile(`^\/posts[\/][0-9a-fA-F]{24}$`)
+	createPostRe  = regexp.MustCompile(`^\/posts[\/]*$`)
+	getPostUserRe = regexp.MustCompile(`^\/posts[\/]users[\/][0-9a-fA-F]{24}$`)
+)
+
 func (h *PostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	switch {
-	case r.Method == http.MethodGet && getUserRe.MatchString(r.URL.Path):
+	case r.Method == http.MethodGet && getPostRe.MatchString(r.URL.Path):
 		h.GetPost(w, r)
 		return
-	case r.Method == http.MethodPost && createUserRe.MatchString(r.URL.Path):
+	case r.Method == http.MethodPost && createPostRe.MatchString(r.URL.Path):
 		h.CreatePost(w, r)
+		return
+	case r.Method == http.MethodGet && getPostUserRe.MatchString(r.URL.Path):
+		h.GetPostUser(w, r)
 		return
 	default:
 		notFound(w, r)
@@ -58,10 +69,31 @@ func (h *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(id)
 
 	post := Post{}
-	err := h.postCollection.FindOne(context.Background(), bson.D{{"_id", id}}).Decode(post)
+	err := h.postCollection.FindOne(context.TODO(), bson.D{{"_id", id}}).Decode(post)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	Writejson(w, r, post)
+}
+
+func (h *PostHandler) GetPostUser(w http.ResponseWriter, r *http.Request) {
+
+	id := r.URL.Path[len("/posts/users/"):]
+	fmt.Println(id)
+
+	postCursor, err := h.postCollection.Find(context.TODO(), bson.D{{"userId", User.ID}})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	posts := Post{}
+	err = postCursor.All(context.TODO(), posts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	Writejson(w, r, posts)
 }
